4802aea3a544df96cbf66b80d5e7b1edae82148d
Philipp Paul <bobbor.pap@googlemail.com>
2013-04-03 21:53:16 +0200

    current update

--- current/src/templates/packer/included_smallloader.js
+++ current/src/templates/packer/included_smallloader.js
@@ -1,1 +1,16 @@
-(function(){if(window.jspackager&&jspackager.jsPath!==undefined&&jspackager.devmode){var curScript=jspackager.jsList["__curScript__"];if(curScript&&curScript.length){for(var j=0,leng=curScript.length;j<leng;j++){if(jspackager.querys['mgnl']){curScript[j]=curScript[j].replace(/\./g,'-');}document.write('<script src="'+jspackager.jsPath+curScript[j]+'.js"><\/script>');}}}})();
+(function($){
+if(window.jspackager && jspackager.jsPath !== undefined && jspackager.devmode){
+	var curScript = jspackager.jsList["__curScript__"];
+	if(curScript && curScript.length){
+		if($ && window.sssl && $(document.scripts || 'script').filter('[src*="__curScript__.js"]')[$.fn.prop ? 'prop' : 'attr']('async')){
+			sssl($.map(curScript, function(src){
+				return jspackager.jsPath+src+'.js';
+			}));
+		} else {
+			for(var j = 0, leng = curScript.length; j < leng; j++){
+				document.write('<script src="'+jspackager.jsPath+curScript[j]+'.js"><\/script>');
+			}
+		}
+	}
+}
+})(window.jQuery);
