ca0b335968be47764337ba1f93275c328a575dda
Philipp Paul <me@philipp-paul.com>
2013-04-03 09:46:03 +0200

    stash change removed

--- nu/src/Resources/js/index.js
+++ nu/src/Resources/js/index.js
@@ -1,7 +1,5 @@
 (function(window, undefined) {
 	"use strict";
-<<<<<<< Updated upstream
-	
 	$script
 		.ready(['jquery', 'scroller'], function() {
 			$(function() {
@@ -13,21 +11,6 @@
 					*/
 					alert('open details');
 					return false;
-=======
-
-	define(['lib/jquery', 'gui/index.enhancements'], function($) {
-		return {
-			init: function() {
-				$(function() {
-					$('.project').on('click', 'button.detail', function() {
-						var details = Ti.UI.createWindow("app://detail.html?foo");
-						details.open();
-						details.setTitle('some project');
-
-						notification.show();
-						return false;
-					});
->>>>>>> Stashed changes
 				});
 			});
 		})
