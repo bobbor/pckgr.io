aeac17972dc009147e49e731afad9a2174d9b3e2
Philipp Paul <me@philipp-paul.com>
2013-04-06 22:52:30 +0200

    basic draft

--- nu/init.js
+++ nu/init.js
@@ -1,1 +1,15 @@
-var
+(function() {
+	var
+		connect = require('connect'),
+		http = require('http'),
+		app
+	;
+
+	app = connect()
+		.use(connect['static']('resource-server'))
+	;
+
+	http.createServer(app).listen(8080, function() {
+		console.log('Running on http://localhost:8080');
+	});
+}());
