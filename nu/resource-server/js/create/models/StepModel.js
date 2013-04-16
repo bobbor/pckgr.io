d187f50f40741657d0906e5b502e394202771315
Philipp Paul <me@philipp-paul.com>
2013-04-17 01:11:39 +0200

    delete unneeded

--- nu/resource-server/js/create/models/StepModel.js
+++
@@ -1,8 +0,0 @@
-(function(window, undefined) {
-	var f = window.Frontender;
-	f.$script.ready(['jquery', '_', 'backbone'], function() {
-		f.StepModel = f.Backbone.Model.extend({
-
-		});
-	});
-}(this, void 0))
