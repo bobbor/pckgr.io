ed60f8c9bf0bf52d3f262405716327c47f47a65e
Philipp Paul <me@philipp-paul.com>
2013-04-17 00:50:30 +0200

    updater

--- nu/src/prog/js/init.js
+++ nu/src/prog/js/init.js
@@ -8,13 +8,12 @@
 		, gui = require('nw.gui')
 		, app = {
 			go: function() {
-				var win = gui.Window.get();
-				win.showDevTools();
-
+				F.inst.mainWindow = gui.Window.get();
 				F.inst.updater = new F.defs.Updater('http://localhost:8080');
-
 				F.inst.updater.on('updateAvailable', function() {
-					F.inst.updater.download();
+					if(confirm('wanna update?')) {
+						F.inst.updater.download();
+					}
 				})
 			},
 			ready: function() {
--- nu/src/prog/js/update/updater.js
+++ nu/src/prog/js/update/updater.js
@@ -9,6 +9,8 @@
 		, Backbone = window.Backbone
 		, version  = new F.defs.FileBridge('prog/version')
 		, fs       = require('fs')
+		, http     = require('http')
+		, p        = require('path')
 		, remote
 	;
 
@@ -18,40 +20,118 @@
 		if(!version.exists) {
 			version.write("4.0.0.0", function() {}, true);
 		}
-		version.read(function(success, data) {
-			if(!success) {
-				that.trigger('read_error');
-			}
-			that.version = JSON.parse(data);
-			remote = new F.defs.HTTPBridge(that.url+'/update?ver='+that.version);
-			that.check();
-		});
+		var check = function() {
+			version.read(function(success, data) {
+				if(!success) {
+					that.trigger('read_error');
+				}
+				that.version = JSON.parse(data);
+				remote = new F.defs.HTTPBridge(that.url+'/update?ver='+that.version);
+				that.check();
+			});
+		};
+
+		if(fs.existsSync('update_in_progress')) {
+			fs.unlinkSync('update_in_progress');
+			$(window).on('load', function() {
+				alert('done');
+			});
+		} else {
+			check();
+		}
 	};
 	_.extend(F.defs.Updater.prototype, Backbone.Events, {
 		check: function(silent) {
 			var that = this;
-			remote.get(function(data) {
-				if(data.length) {
-					that.updateData = JSON.parse(data);
-					that.trigger('updateAvailable')
-				}
-			});
+			if(that.version !== 'NIGHTLY') {
+				remote.get(function(data) {
+					if(data.length) {
+						that.updateData = JSON.parse(data);
+						if(!silent) {
+							that.trigger('updateAvailable')
+						}
+					}
+				});
+			}
 		},
 		download: function() {
 			if(_.keys(this.updateData.to_add).length) {
-				this.recursive_download(this.updateData.to_add, 'prog/temp', '')
+				this.recursive_download(this.updateData.to_add, 'prog')
 			}
 		},
+		_download: function(from, to, fn) {
+			http.get(from, function(res) {
+				var offset = 0;
+				var data = new Buffer(0);
+				res.on('data', function(chunk) {
+					data = Buffer.concat([data, chunk]);
+				}).on('end', function() {
+					fs.writeFile(to, data, fn);
+				})
+			});
+		},
 		recursive_download: function(obj, target, extra) {
-			for(var prop in obj) {
-				if(_.isArray(obj[prop])) {
-					for(var i = 0;i<obj[prop].length;i++) {
-						console.log(target+extra+'/'+obj[prop][i], this.url+'/download'+extra+'/'+obj[prop][i]);
+			var url = this.url + this.updateData.path;
+			var that = this;
+			if(!fs.existsSync(target)) {
+				fs.mkdirSync(target);
+			}
+			var arrIterator = function(arr, extra, fncomplete) {
+				if(!arr.length) {
+					fncomplete();
+					return;
+				}
+				var item = arr.shift();
+				that._download(url+extra+'/'+item, target+extra+'/'+item, function() {
+					arrIterator(arr, extra, fncomplete);
+				});
+			};
+
+			var objIterator = function(keys, obj, extra, fncomplete) {
+				if(!keys.length) {
+					fncomplete()
+					return;
+				}
+				var key = keys.shift();
+				var item = obj[key];
+				var e = extra+'/'+key;
+
+				if(_.isArray(item)) {
+					arrIterator(item, extra, function() {
+						objIterator(keys, obj, extra, fncomplete)
+					});
+				} else {
+					if(!fs.existsSync(target+extra+'/'+key)) {
+						fs.mkdirSync(target+extra+'/'+key);
 					}
+					objIterator(_.keys(item), item, e, function() {
+						objIterator(keys, obj, extra, fncomplete)
+					});
 				}
-				else {
-					this.recursive_download(obj[prop], target, '/'+prop)
+			};
+			objIterator(_.keys(obj), obj, '', function() {
+				fs.writeFile('update_in_progress', '');
+				version.write(JSON.stringify(that.updateData.version), function() {}, true);
+				F.inst.mainWindow.reload();
+			});
+		},
+		_copyExistingUpdate: function(src, target, cb) {
+			var files = fs.readdirSync(src);
+			var stat;
+			for(var i = 0; i < files.length; i++) {
+				stat = fs.statSync(src+'/'+files[i]);
+				if(stat.isDirectory()) {
+					if(!fs.existsSync(target+'/'+files[i])) {
+						fs.mkdirSync(target+'/'+files[i]);
+					}
+					this._copyExistingUpdate(src+'/'+files[i], target+'/'+files[i]);
+				} else if(stat.isFile()) {
+					fs.renameSync(src+'/'+files[i], target+'/'+files[i]);
 				}
+			}
+			fs.rmdirSync(src);
+			if(cb) {
+				cb();
 			}
 		}
 	});
